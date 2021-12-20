3f5b36a9ac5e1a86e7e62d2e2b529a06e02d600e
t18j057ny <74063166+t18j057ny@users.noreply.github.com>
2021-12-20 21:08:43 +0900

    Update search_ef.js

--- js/search_ef.js
+++ js/search_ef.js
@@ -1,5 +1,5 @@
 $('#query').focus(function(){
-    $('#l_query').animate({'color': 'rgb(255, 153, 0)'}, 500);
+    $('#l_query').animate({'color': 'rgb(255, 187, 41)'}, 500);
 }).blur(function(){
     $('#l_query').animate({'color': 'white'}, 500);
 });
