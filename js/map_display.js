8378636fd20bdea921018ae8a24351636f92147a
t18j057ny <74063166+t18j057ny@users.noreply.github.com>
2021-12-20 14:49:04 +0900

    Update map_display.js

--- js/map_display.js
+++ js/map_display.js
@@ -139,6 +139,7 @@ function grass_map(id) {
     var id = Number(id);
     map.remove();
     map = null;
+    map = L.map('map').setView([36.575,135.984], 5);  
     var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
       attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
       maxZoom: 19
